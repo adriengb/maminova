32ee991c58f0e6f553fd4eaf9634882e51169294
adrien-giraud <adrien.giraud@axa.com>
2018-02-16 22:35:29 +0100

    add c24 spyder

--- model/image_sorter.py
+++ model/image_sorter.py
@@ -1,5 +1,4 @@
 import multiprocessing
-import glob
 from functools import partial
 import random
 import pandas as pd
@@ -15,28 +14,35 @@ class ImageSorter(object):
         self.seed = seed
         self.labels = pd.read_csv('../data/cleaned_labels.csv')
 
-    def sort(self):
-        imagesList = glob.glob("../data/raw/*.jpg")
+    def sort(self, min_volume=400):
         random.seed(self.seed)
-        random.shuffle(imagesList)
-        split_index = int(len(imagesList)*self.train)
-        train_images = imagesList[:split_index]
-        test_images = imagesList[split_index:]
-        pool = multiprocessing.Pool(multiprocessing.cpu_count())
-        pool.map(partial(self.__sort__, mode='train'), train_images)
-        pool.map(partial(self.__sort__, mode='predict'), test_images)
-        pool.close()
+        brands = pd.DataFrame(self.labels.cleaned_brand.value_counts())
+        accepted_brands = list(brands[brands.cleaned_brand > min_volume].index)
+        for brand in accepted_brands:
+            directory_train = '../model/data_model/train/{}'.format(brand)
+            directory_validation = '../model/data_model/validation/{}'.format(brand)
+            if not os.path.exists(directory_train):
+                os.makedirs(directory_train)
+            if not os.path.exists(directory_validation):
+                os.makedirs(directory_validation)
+            imagesList = list(self.labels[self.labels.cleaned_brand==brand].image_name)
+            random.shuffle(imagesList)
+            split_index = int(len(imagesList) * self.train)
+            train_images = imagesList[:split_index]
+            test_images = imagesList[split_index:]
+            pool = multiprocessing.Pool(multiprocessing.cpu_count())
+            pool.map(partial(self.__sort__, mode='train', brand=brand), train_images)
+            pool.map(partial(self.__sort__, mode='validation', brand=brand), test_images)
+            pool.close()
 
-    def __sort__(self, img_path, mode):
-        img_name = img_path.split('/')[-1]
-        brand = list(self.labels.cleaned_brand[self.labels.image_name==img_name])
-        if brand:
-            directory = '../model/{}/{}'.format(mode, brand[0])
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            dst = '{}/{}'.format(directory, img_name)
+    def __sort__(self, img_name, mode, brand):
+        img_path = '../data/raw/{}'.format(img_name)
+        dst = '../model/data_model/{}/{}/{}'.format(mode, brand,img_name)
+        try:
             shutil.copy(img_path, dst)
+        except:
+            print(img_path)
 
 if __name__ == '__main__':
-    transformer = ImageSorter(train_size=0.7)
+    transformer = ImageSorter(train_size=0.8)
     transformer.sort()
---
+++ watchbot/spiders/c24.py
@@ -0,0 +1,27 @@
+import scrapy
+import re
+from watchbot.items import WatchPicture
+
+
+class C24pider(scrapy.Spider):
+    name = "c24"
+    start_urls = ['https://www.chrono24.fr/rolex/index.htm']
+    #counter = 1
+
+    def parse(self, response):
+        # loop over all watches link elements that link off  and yield a request to grab the watch
+        # data and images
+        for div in response.xpath("//div[@class='article-item-container']"):
+            yield scrapy.Request("https://www.chrono24.fr/{}".format(div.xpath("./a/@href").extract()[0]), self.parse_watch)
+        #counter += 1
+        #next = ['https://www.chrono24.fr/rolex/index-{}.htm'.format(counter)]
+        #yield scrapy.Request(next, self.parse)
+
+    def parse_watch(self, response):
+        # grab the URL of the cover image
+        title = response.xpath("//h1[@class='h3 m-t-0']/text()").extract()[0].lower()
+        price = response.xpath("//tr[td[1]/strong/text()='Prix']/td[2]/text()").extract()
+        print('PRICE')
+        print(price)
+        yield WatchPicture(title=title, price=price)
+
