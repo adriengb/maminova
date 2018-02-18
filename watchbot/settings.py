5f25995029cd359ec6b116168275fdd477bca31d
adrien <adriengb@gmail.com>
2018-02-18 14:54:26 +0100

    update c24 scraper

--- watchbot/settings.py
+++ watchbot/settings.py
@@ -22,15 +22,17 @@ NEWSPIDER_MODULE = 'watchbot.spiders'
 #ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 1
+
+
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 1
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
@@ -56,7 +58,10 @@ COOKIES_ENABLED = False
 USER_AGENT_LIST = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7",
     "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0) Gecko/16.0 Firefox/16.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10"]
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13"
+]
+
 HTTP_PROXY = 'http://127.0.0.1:8123'
 DOWNLOADER_MIDDLEWARES = {
      'watchbot.middlewares.RandomUserAgentMiddleware': 400,
@@ -86,12 +91,12 @@ AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 100
+AUTOTHROTTLE_MAX_DELAY = 40
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 0.3
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- watchbot/spiders/c24.py
+++ watchbot/spiders/c24.py
@@ -1,35 +1,56 @@
 import scrapy
-import re
 from watchbot.items import WatchPicture
-
 
 class C24pider(scrapy.Spider):
     name = "c24"
-    start_urls = ['https://www.chrono24.com/rolex/index.htm']
-    #counter = 1
+    brands = ['rolex', 'omega', 'jaeger', 'iwc', 'patek', 'panerai', 'seiko', 'cartier', 'hublot', 'breitling', 'zenith', 'tagheuer', 'tudor',
+              'audemarspiguet', 'breguet', 'blancpain', 'chanel', 'alangesoehne']
+    start_urls = ['https://www.chrono24.com/{}/index-1.htm?sortorder=5'.format(brand) for brand in brands]
+    count = 1
 
     def parse(self, response):
         # loop over all watches link elements that link off  and yield a request to grab the watch
         # data and images
-        for div in response.xpath("//div[@class='article-item-container']"):
-            yield scrapy.Request("https://www.chrono24.com/{}".format(div.xpath("./a/@href").extract()[0]), self.parse_watch)
-        #counter += 1
-        #next = ['https://www.chrono24.fr/rolex/index-{}.htm'.format(counter)]
-        #yield scrapy.Request(next, self.parse)
+        if len(response.xpath("//div[@class='article-list block']/div[@class='article-item-container']").extract())>0:
+            for div in response.xpath("//div[@class='article-list block']/div[@class='article-item-container']"):
+                yield scrapy.Request("https://www.chrono24.com/{}".format(div.xpath("./a/@href").extract()[0]), self.parse_watch)
+            self.count += 1
+            next = 'https://www.chrono24.com/rolex/index-{}.htm?sortorder=5'.format(self.count)
+            yield scrapy.Request(next, self.parse)
 
     def parse_watch(self, response):
         # grab the URL of the cover image
-        title = response.xpath("//h1[@class='h3 m-t-0']/text()").extract()[0].lower()
-        price = re.findall(r'\$(.*)', response.xpath("//tr[td[1]/strong/text()='Price']/td[2]/text()").extract()[0])[0]
-        brand = response.xpath("//tr[td[1]/strong/text()='Brand']/td[2]/a/@title").extract()[Ø]
-        model = response.xpath("//tr[td[1]/strong/text()='Model']/td[2]/text()").extract()[0]
-        ref = response.xpath("//tr[td[1]/strong/text()='Ref.No']/td[2]/text()").extract()[0]
-        movement = response.xpath("//tr[td[1]/strong/text()='Movement']/td[2]/text()").extract()[0]
-        case_material = response.xpath("//tr[td[1]/strong/text()='Case material']/td[2]/text()").extract()[0]
-        bracelet_material = response.xpath("//tr[td[1]/strong/text()='Bracelet material']/td[2]/text()").extract()[0]
-        condition = response.xpath("//tr[td[1]/strong/text()='Condition']/td[2]/text()").extract()[0]
-        gender = response.xpath("//tr[td[1]/strong/text()='Gender']/td[2]/text()").extract()[0]
-        location = response.xpath("//tr[td[1]/strong/text()='Location']/td[2]/text()").extract()[0]
-        yield WatchPicture(title=title, price=price, brand=brand, model=model, ref=ref, movement=movement, case_material=case_material,
-                           bracelet_material=bracelet_material, condition=condition, gender=gender, location=location)
+        title = response.xpath("//h1[@class='h3 m-t-0']/text()").extract()[0].lower().strip()
+        brand = response.xpath("//tr[td[1]/strong/text()='Brand']/td[2]/a/@title").extract()[0].lower().strip()
+        try:
+            price = ''.join(response.xpath("//tr[td[1]/strong/text()='Price']/td[2]/text()").extract()).lower().strip()
+        except:
+            price = ''
+        try:
+            model = response.xpath("//tr[td[1]/strong/text()='Model']/td[2]/a/text()").extract()[0].lower().strip()
+        except:
+            model = ''
+        try:
+            ref = response.xpath("//tr[td[1]/strong/text()='Ref. No.']/td[2]/a/text()").extract()[0].lower().strip()
+        except:
+            ref = ''
+        try:
+            condition = response.xpath("//tr[td[1]/strong/text()='Condition']/td[2]/text()").extract()[0].lower().strip()
+        except:
+            condition = ''
+        try:
+            location = response.xpath("//tr[td[1]/strong/text()='Location']/td[2]/text()").extract()[0].lower().strip()
+        except:
+            location = ''
+        try:
+            year = response.xpath("//tr[td[1]/strong/text()='Year']/td[2]/text()").extract()[0].lower().strip()
+        except:
+            year = ''
+        images = response.xpath("//div[@class='detail-images']//div/@data-original").extract()
+        image_urls = [im.split('?')[0] for im in images]
+        for image_url in image_urls:
+            image_name = image_url.split('/')[-1]
+            yield WatchPicture(title=title, price=price, brand=brand, model=model,
+                               ref=ref, condition=condition, location=location,
+                               image_name=image_name, image_urls=[image_url], year=year)
 
